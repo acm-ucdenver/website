bcde5fdebe3727f68addd681c3700a34b95a3c0b
Wmaxlees <william.lees@ucdenver.edu>
2015-05-29 21:13:08 -0600

    Created step definitions for features

---
+++ server/features/step_definitions/get-event-block.steps.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function() {
+
+	this.Given(/^I do not have access to the \/event API$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Given(/^I send a GET call to the \/event API$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+	this.Given(/^I sends a GET call to the \/event\/\{event\-id\} API$/, function (callback) {
+		// Write code here that turns the phrase above into concrete actions
+		callback.pending();
+	});
+
+};
