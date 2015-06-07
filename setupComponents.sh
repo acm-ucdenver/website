fd2fee3a828b6211de7b5ea8ccb67958c0f89abe
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-06-06 22:59:52 -0600

    removed extreneous stubs, integrated mongoose, clean up bash scripts, added bootstrap to base page layout

---
+++ setupComponents.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# required to run each time you are working with a new clone of the site
+# should not require admin / super user role
+# on linux, you can run this command to fix if npm doesn't have the correct permissions:
+# sudo chown -R $(whoami) ~/.npm
+
+echo 'Installing node project packages'
+cd site
+npm install
+echo 'Installing web framework packages'
+cd ..
+bower install
