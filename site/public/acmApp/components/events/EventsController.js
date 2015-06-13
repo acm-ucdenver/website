23efbb407e658b50ec55bb60e86a8f132622723a
mfscheidler <mattscheidler@outlook.com>
2015-06-13 13:29:49 -0600

    updated events at index.html

--- site/public/acmApp/components/events/EventsController.js
+++ site/public/acmApp/components/events/EventsController.js
@@ -6,3 +6,39 @@
 
 // 'eventsDirective'
 //   returns template for display of event object
+
+angular.module('acmApp')
+	.controller('EventsController', addEvents )
+	.directive('eventView', function() {
+		return {
+			templateUrl: '/acmApp/components/events/events.html' // '<tr><td>{{event.title}}</td></tr>'//
+		};
+	})
+	
+	function addEvents() {
+		this.events = [
+			{
+				title: 'Event Name',
+				startDate: 'Date',
+				endDate: 'Date',
+				description: 'Description of event',
+				location: 'Location of event',
+				rsvp: false,
+				imgs: [
+					{
+						src: 'Address of image'
+					}
+				]
+			}
+		];
+	};
+	/* .factory('EventModel', function() {
+		var events;
+		$http.get('/api/event', function(req, res) {
+			if (err) 
+			{ throw err; }
+			else {
+				events = res.body;
+			}
+		});
+	}); */
