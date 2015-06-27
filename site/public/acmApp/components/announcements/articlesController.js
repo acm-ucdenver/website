0f87b8bedcc68326a19a4099414f3b9ed37d5184
NicCranon <nicole.cranon@gmail.com>
2015-06-27 16:11:10 -0600

    removed 'http://localhost:3000' to generalize

--- site/public/acmApp/components/announcements/articlesController.js
+++ site/public/acmApp/components/announcements/articlesController.js
@@ -2,7 +2,7 @@ angular
 	.module('acmApp')
 	.controller('ArticlesController', ['$scope', '$http', function($scope, $http){
 		// Get all articles
-		$http.get('http://localhost:3000/articles')
+		$http.get('/articles')
 	        .success(function(data) {
 	            $scope.articles = data;
 	        });
