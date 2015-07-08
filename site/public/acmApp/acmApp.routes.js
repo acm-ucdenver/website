6016718c3f4448a15aaa91d5cf20d152d04930fa
W. Max Lees <william.lees@echostar.com>
2015-07-07 22:46:47 -0600

    Articles can now be hidden

--- site/public/acmApp/acmApp.routes.js
+++ site/public/acmApp/acmApp.routes.js
@@ -2,9 +2,6 @@ acmApp.config(['$stateProvider', '$locationProvider', function($stateProvider, $
     $stateProvider.state('index', {
         url: "",
         views: {
-            'content': { 
-                templateUrl: 'acmApp/partials/index.html',
-            },
             'navbar': {
                 templateUrl: 'acmApp/components/navbar/navbar.html',
                 controller: 'NavBarController'
@@ -14,11 +11,9 @@ acmApp.config(['$stateProvider', '$locationProvider', function($stateProvider, $
     .state('index.main', {
         url: "/index",
         views: {
-            'events': { 
-                templateUrl: 'acmApp/components/events/events.html',
-            },
-            'articles': {
+            'content@': { 
                 templateUrl: 'acmApp/components/articles/articles.html',
+                controller: 'ArticlesController as controller'
             },
         }
     });
--- site/public/acmApp/components/articles/articleDirective.js
+++ site/public/acmApp/components/articles/articleDirective.js
@@ -1,15 +1,51 @@
 (function () {
     'use strict';
 
-    angular.module('acmApp').directive('articleframe', ArticlesDirective);
+    angular.module('acmApp').directive('articleframe', ['$timeout', '$interval', ArticlesDirective]);
 
-    function ArticlesDirective() {
+    function ArticlesDirective($timeout, $interval) {
+        function link (scope, element, attrs) {
+            var show = true;
+            var full_text = scope.info.body;
+            var original_height = 0;
+            var height = 0;
+
+            $timeout(init, false);
+            function init(){
+                original_height = element[0].offsetHeight;
+                height = original_height;
+            }
+
+            element.bind('click', function(event) {
+                show = !show;
+                if (show) {
+                    scope.info.body = full_text;
+                    scope.$apply();
+                    $interval(function () {
+                        height += (original_height-120)/27;
+                        element.css('height', height+'px'); 
+                    }, 20, 27);
+                } else {
+                    $interval(function () {
+                        height -= (original_height-120)/27;
+                        element.css('height', height+'px'); 
+                    }, 20, 27);
+                    $timeout(function() {
+                        scope.info.body = full_text.substring(0, 150) + "...";
+                        scope.$apply();
+                    }, 20*27);
+                }
+            })
+        }
+
         return {
             restrict: 'E',
+            link: link,
+            replace: true,
             scope: {
                 info: '='
             },
-            templateUrl: 'article_template.html'
+            templateUrl: 'acmApp/components/articles/article_template.html'
         }
     }
 })();
